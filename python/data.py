c01bb127d9735377da79089c4e516e0e05fce5d8
Edward-Billington <zark.youtube@hotmail.com>
2020-04-05 23:37:51 +0100

    Data mining completed

--- python/data.py
+++ python/data.py
@@ -2,6 +2,7 @@
 
 import os
 path_to_darknet = "/home/edward/Documents/darknet/"
+images_count = 17
 
 files = {
 	"yolov3-voc_final.weights": "yolov3-voc.cfg",
@@ -10,14 +11,15 @@ files = {
 	"yolov3-voc-v4_final.weights": "yolov3-voc-v4.cfg",
 }
 
-images = [
-	"knife_image_1.jpg"
-]
-
 for file in files:
 	weight_file = path_to_darknet + "backup/" + file
 	config_file = path_to_darknet + "cfg/" + files[file]
-	os.system("cat "+config_file)
-	for a in images:
-		print(a)
-	break
+
+	# GO THROUGH EACH MODEL
+	for i in range(1, images_count+1):
+		# number 7 is png
+		filetype = ".jpg" if i != 7 else ".png"
+		filepath = path_to_darknet + "data/testing_images/knife_image_" + str(i) + filetype
+		cmd = "./darknet detector test cfg/voc.data " + config_file + " " + weight_file + " " + filepath
+		out = path_to_darknet + "results/" + files[file]+"_image_" + str(i) +".txt"
+		os.system(cmd + " > " + out)
